35e9a1fe129bf89099e5f92b75456972efa65c5c
yaw96_ <yaw.networks@gmail.com>
2020-04-28 22:40:39 +0700

    add open free public REST API

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const todoRoutes = require("./todoRoutes.js");
 const userRoutes = require("./userRoutes.js");
+const checkIpRoutes = require("./checkIpRoutes.js");
 
 router.use("/", userRoutes);
 router.use("/todos", todoRoutes);
+router.use("/checkip", checkIpRoutes);
 
 module.exports = router;

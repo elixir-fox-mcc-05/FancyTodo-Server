eb451f2be8bff49b221f9def0c88188b531f7249
IrwanKhaliq <Irwan.khaliq10@gmail.com>
2020-04-29 17:53:07 +0700

    install cors

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
  if(process.env.NODE_ENV == 'development') require('dotenv').config()
 
-const errorHandlers = require('./helpers/error_handler')
+const cors = require('cors')
 const express = require('express')
 const routes = require('./routes')
+const errorHandlers = require('./helpers/error_handler')
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
+app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 

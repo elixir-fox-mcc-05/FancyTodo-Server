24834558d2a1a7043b26651301de4690477adefb
chairulakmal <chairulakmal@live.com>
2020-04-29 21:53:36 +0700

    added cors for cross-port client-server comms

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ if (process.env.NODE_ENV === 'development') {
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const cors = require('cors')
 const router = require('./routers')
 
 app.set('port', process.env.PORT)
+
+app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 

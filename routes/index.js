26116df4891d054a41f7770baa8dcab54cea78ce
yaw96_ <yaw.networks@gmail.com>
2020-04-29 21:27:20 +0700

    add a new free public api

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const todoRoutes = require("./todoRoutes.js");
 const userRoutes = require("./userRoutes.js");
 const checkIpRoutes = require("./checkIpRoutes.js");
+const getRandomQuote = require("./getRandomQuote.js");
 
 router.use("/", userRoutes);
 router.use("/todos", todoRoutes);
 router.use("/checkip", checkIpRoutes);
+router.use("/randomquote", getRandomQuote);
 
 module.exports = router;

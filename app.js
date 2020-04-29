f0fb4f36d6cb8c63078a532c8555159f8c61c2e2
yaw96_ <yaw.networks@gmail.com>
2020-04-29 21:28:54 +0700

    add cors

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ if (process.env.NODE_ENV === "development") {
 }
 
 const express = require("express");
+const cors = require("cors");
 const router = require("./routes/index.js");
 const errHandler = require("./middlewares/errHandler.js");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.urlencoded({ extended : true }));
 app.use(express.json());
 

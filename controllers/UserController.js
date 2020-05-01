ec735c02d6162579a69f126046dd6e969513fe4f
HafidhFinand <hafidh.finand@gmail.com>
2020-05-01 09:17:10 +0700

    add google sign in

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,6 +1,7 @@
 const { User } = require('../models');
 const { compare } = require('../helpers/bycrypt.js');
 const { generateToken } = require('../helpers/jsonwebtoken.js');
+const googleVerification = require('../helpers/googleOauthApi.js');
 
 class UserController {
     static findAll(req, res, next) {
@@ -61,6 +62,43 @@ class UserController {
                     name: 'Internal Server Error',
                     errors: [{ message: err }]
                 })
+            })
+    }
+
+    static googleLogin(req, res, next) {
+        let email = null;
+        let google_token = req.headers.google_token;
+        let newUser = false;
+        let name = null;
+        googleVerification(google_token)
+            .then(payload => {
+                email = payload.email;
+                name = payload.name;
+                return User
+                    .findOne({ where: { email } })
+            })
+            .then(user => {
+                if (user) {
+                    return user;
+                } else {
+                    newUser = true;
+                    return User
+                        .create({
+                            name,
+                            email,
+                            password: process.env.DEFAULT_GOOGLE_PASSWORD
+                        })
+                }
+            })
+            .then(user => {
+                let code = newUser ? 201 : 200;
+                const token = generateToken({ id: user.id, name: user.name, email: user.email })
+                res.status(code).json({
+                    token
+                })
+            })
+            .catch(err => {
+                next(err)
             })
     }
 }
--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -4,6 +4,7 @@ const UserController = require('../controllers/UserController.js');
 
 router.post('/signin', UserController.signin);
 router.post('/signup', UserController.signup);
+router.post('/google-login', UserController.googleLogin);
 router.get('/', UserController.findAll);
 
 module.exports = router;

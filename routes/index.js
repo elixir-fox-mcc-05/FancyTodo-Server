c19d9a1044449019b71f4bf8cad80c1f3bb98541
HafidhFinand <hafidh.finand@gmail.com>
2020-05-02 07:59:02 +0700

    add second 3rd api

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const usersRoute = require('./usersRoute.js');
 const todosRoute = require('./todosRoute.js');
 const animeRoute = require('./animeRoute.js');
+const coronaRoute = require('./coronaRoute.js');
 
+router.use('/corona', coronaRoute);
 router.use('/todos', todosRoute);
 router.use('/users', usersRoute);
 router.use('/anime', animeRoute);
